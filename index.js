d09a09b18af3c9525c10b9fb4dfb6dac55a18da1
Kakon Debnath <kakondebnath96@gmail.com>
2023-06-07 21:30:16 +0600

    add user collection api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 const corsOptions = {
@@ -14,9 +15,51 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to summer school!');
 });
+async function run() {
+    try {
+        // all collections
+        const usersCollection = client.db("summerSchoolDB").collection("users");
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+        // Save user email and role in DB
+        app.put('/users', async (req, res) => {
+            const user = req.body
+            const email = user.email
+            const query = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, options)
+            res.send(result)
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 
 app.listen(port, (req, res) => {
