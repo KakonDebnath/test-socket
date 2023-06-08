75aa2e33670a9873d8ac448d792f2c997009743f
Kakon Debnath <kakondebnath96@gmail.com>
2023-06-08 10:46:15 +0600

    add jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -15,8 +16,29 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    // bearer token
+    const token = authorization.split(' ')[1]
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded
+      next()
+    })
+  }
 
-const uri = process.env.MONGODB_URI;
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9m7cjb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,8 +56,17 @@ async function run() {
     try {
         // all collections
         const usersCollection = client.db("summerSchoolDB").collection("users");
+        const classesCollection = client.db("summerSchoolDB").collection("classes");
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
+
+        // sign jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      
+            res.send({ token })
+          })
         // Save user email and role in DB
         app.put('/users', async (req, res) => {
             const user = req.body
@@ -47,6 +78,12 @@ async function run() {
             }
             const result = await usersCollection.updateOne(query, updateDoc, options)
             res.send(result)
+        })
+
+        // Add A Class
+        app.post("/addClass", async (req, res) => {
+            const classes = req.body;
+            console.log(classes);
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
